958fb86f9ca76ccd208e691af70238b1da88e6c5
Xavier Haniquaut <xavhan@hotmail.com>
2018-07-25 10:55:35 +0200

    1.0.5

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
 var _yerror = require('yerror');
 
@@ -59,6 +59,7 @@ function preferencesGet(preferences, name) {
   var res = preferences.filter(function (pref) {
     return pref.name === name;
   })[0];
+
   return 'undefined' !== typeof res ? res.value : {}.undef;
 }
 
